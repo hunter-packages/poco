91bae5e8459af5b5490f5f257169bac56348d85e
Himanshu Shekhar <Himanshu.Shekhar@jp.sony.com>
2018-02-20 15:44:19 +0900

    PocoConfig should find sqlite3 and ZLIB when hunter is enabled.

--- Foundation/cmake/PocoFoundationConfig.cmake
+++ Foundation/cmake/PocoFoundationConfig.cmake
@@ -1,1 +1,8 @@
 include("${CMAKE_CURRENT_LIST_DIR}/PocoFoundationTargets.cmake")
+
+if (@POCO_UNBUNDLED@)
+    if (@HUNTER_ENABLED@)
+        hunter_add_package(ZLIB)
+        find_package(ZLIB CONFIG REQUIRED)
+    endif()
+endif()
